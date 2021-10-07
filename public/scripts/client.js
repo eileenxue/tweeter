29fa83cdc8e254bfc1f0f42abf25775beac851d7
Eileen Xue <xue.eileen@gmail.com>
2021-10-07 01:45:15 +0000

    Render tweets from data list

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,7 @@
 $(document).ready(function() {
 
   const createTweetElement = function (data) {
-    const newTweet = 
+    const $newTweet = 
     `
       <article class="tweet-post">
       <header>
@@ -28,27 +28,49 @@ $(document).ready(function() {
       </footer>
       </article>
     `
-    return newTweet;
+    return $newTweet;
   };
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  // Fake data taken from initial-tweets.json
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  // Render Tweets
+  const renderTweets = function(tweets) {
+    // Empty out the tweet container in the beginning
+    $("#tweets-container").empty();
+    
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (tweet of tweets){
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet); 
+    }
   }
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log('This is the new tweet');
-  console.log($tweet); // to see what it looks like
-$('#tweets-container').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  
+  renderTweets(tweetData);
 
 });
