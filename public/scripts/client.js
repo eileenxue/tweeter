b7f03fb08cd41377cc16d8c765313b98733b56ae
Eileen Xue <xue.eileen@gmail.com>
2021-10-07 02:44:09 +0000

    Fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,48 +31,17 @@ $(document).ready(function() {
     return $newTweet;
   };
 
-  // Fake data taken from initial-tweets.json
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   // Render Tweets
   const renderTweets = function(tweets) {
-    // Empty out the tweet container in the beginning
     $("#tweets-container").empty();
-    
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
+    // Loop through tweets and then append to container
     for (tweet of tweets){
       const $tweet = createTweetElement(tweet);
       $('#tweets-container').prepend($tweet); 
     }
-  }
-  
-  renderTweets(tweetData);
-
+  };
+  //renderTweets(tweetData);
 
   // Add Event Listener to Form's Submit Button
   $('form').submit(function (event) {
@@ -86,5 +55,14 @@ $(document).ready(function() {
     });
 
   });
+
+  // Load Tweets
+  const loadTweets = function (){
+    $.get('/tweets', function (data) {
+      renderTweets(data);
+    })
+  };
+
+  loadTweets();
 
 });
